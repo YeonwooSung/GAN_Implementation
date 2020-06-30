42bf9e3e43e33e64fc88a49a99392f323bc5fe8e
YeonwooSung <neos960518@gmail.com>
2020-06-30 20:41:24 +0900

    Fix minor errors

--- src/sagan/sagan.py
+++ src/sagan/sagan.py
@@ -76,10 +76,10 @@ class SpectralNorm(nn.Module):
 #------------------------------------------------------
 
 
-class Self_Attn(nn.Module):
+class SelfAttention(nn.Module):
     """ Self attention Layer"""
     def __init__(self,in_dim,activation):
-        super(Self_Attn,self).__init__()
+        super(SelfAttention, self).__init__()
         self.chanel_in = in_dim
         self.activation = activation
         
@@ -88,7 +88,8 @@ class Self_Attn(nn.Module):
         self.value_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim , kernel_size= 1)
         self.gamma = nn.Parameter(torch.zeros(1))
 
-        self.softmax  = nn.Softmax(dim=-1) #
+        self.softmax  = nn.Softmax(dim=-1)
+
     def forward(self,x):
         """
             inputs :
@@ -97,18 +98,24 @@ class Self_Attn(nn.Module):
                 out : self attention value + input feature 
                 attention: B X N X N (N is Width*Height)
         """
-        m_batchsize,C,width ,height = x.size()
-        proj_query  = self.query_conv(x).view(m_batchsize,-1,width*height).permute(0,2,1) # B X CX(N)
-        proj_key =  self.key_conv(x).view(m_batchsize,-1,width*height) # B X C x (*W*H)
-        energy =  torch.bmm(proj_query,proj_key) # transpose check
-        attention = self.softmax(energy) # BX (N) X (N) 
-        proj_value = self.value_conv(x).view(m_batchsize,-1,width*height) # B X C X N
+        m_batchsize, C, width, height = x.size()
 
-        out = torch.bmm(proj_value,attention.permute(0,2,1) )
+        N = width * height
+
+        proj_query  = self.query_conv(x).view(m_batchsize, -1, N).permute(0,2,1) # B X C X N
+        proj_key =  self.key_conv(x).view(m_batchsize, -1, N) # B X C x N
+
+        energy =  torch.bmm(proj_query, proj_key) # transpose check
+        attention = self.softmax(energy) # BX (N) X (N)
+
+        proj_value = self.value_conv(x).view(m_batchsize, -1, N) # B X C X N
+
+        out = torch.bmm(proj_value,attention.permute(0,2,1))
         out = out.view(m_batchsize,C,width,height)
         
-        out = self.gamma*out + x
+        out = self.gamma * out + x
         return out,attention
+
 
 class Generator(nn.Module):
     """Generator."""
@@ -156,18 +163,20 @@ class Generator(nn.Module):
         last.append(nn.Tanh())
         self.last = nn.Sequential(*last)
 
-        self.attn1 = Self_Attn( 128, 'relu')
-        self.attn2 = Self_Attn( 64,  'relu')
+        self.attn1 = SelfAttention(128, 'relu')
+        self.attn2 = SelfAttention(64, 'relu')
 
     def forward(self, z):
         z = z.view(z.size(0), z.size(1), 1, 1)
-        out=self.l1(z)
-        out=self.l2(out)
-        out=self.l3(out)
-        out,p1 = self.attn1(out)
-        out=self.l4(out)
-        out,p2 = self.attn2(out)
-        out=self.last(out)
+
+        out = self.l1(z)
+        out = self.l2(out)
+        out = self.l3(out)
+        out, p1 = self.attn1(out)
+        if self.l4:
+            out = self.l4(out)
+        out, p2 = self.attn2(out)
+        out = self.last(out)
 
         return out, p1, p2
 
@@ -201,7 +210,8 @@ class Discriminator(nn.Module):
             layer4.append(SpectralNorm(nn.Conv2d(curr_dim, curr_dim * 2, 4, 2, 1)))
             layer4.append(nn.LeakyReLU(0.1))
             self.l4 = nn.Sequential(*layer4)
-            curr_dim = curr_dim*2
+            curr_dim = curr_dim * 2
+
         self.l1 = nn.Sequential(*layer1)
         self.l2 = nn.Sequential(*layer2)
         self.l3 = nn.Sequential(*layer3)
@@ -209,16 +219,17 @@ class Discriminator(nn.Module):
         last.append(nn.Conv2d(curr_dim, 1, 4))
         self.last = nn.Sequential(*last)
 
-        self.attn1 = Self_Attn(256, 'relu')
-        self.attn2 = Self_Attn(512, 'relu')
+        self.attn1 = SelfAttention(256, 'relu')
+        self.attn2 = SelfAttention(512, 'relu')
 
     def forward(self, x):
         out = self.l1(x)
         out = self.l2(out)
         out = self.l3(out)
-        out,p1 = self.attn1(out)
-        out=self.l4(out)
-        out,p2 = self.attn2(out)
-        out=self.last(out)
+        out, p1 = self.attn1(out)
+        if self.l4:
+            out = self.l4(out)
+        out, p2 = self.attn2(out)
+        out = self.last(out)
 
         return out.squeeze(), p1, p2
